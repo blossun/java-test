349534dace64945e943bf0e1c57a8b0a959cd60c
blossun <dnsngu@gmail.com>
2020-12-17 16:29:49 +0900

    ch01 : 테스트 이름 표시하기
    **@DisplayNameGeneration**
    * Method와 Class 레퍼런스를 사용해서 테스트 이름을 표기하는 방법 설정.
    * 기본 구현체로 ReplaceUnderscores 제공
    
    **@DisplayName**
    * 어떤 테스트인지 테스트 이름을 보다 쉽게 표현할 수 있는 방법을 제공하는 애노테이션
    * @DisplayNameGeneration 보다 우선 순위가 높다.
    
    issue #1

--- src/test/java/dev/solar/mythejavatest/StudyTest.java
+++ src/test/java/dev/solar/mythejavatest/StudyTest.java
@@ -4,16 +4,19 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
     @Test
-    void create() {
+    @DisplayName("스터디 만들기 ╯°□°）╯ ")
+    void create_new_study() {
         System.out.println("create");
         Study study = new Study();
         assertNotNull(study);
     }
 
     @Test
-    void create1() {
+    @DisplayName("스터디 만들기 \uD83D\uDE31")
+    void create_new_study_again() {
         System.out.println("create1");
     }
 
