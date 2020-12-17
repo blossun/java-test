4b397b1061dd6af2a0de8a40833ea9071860b995
blossun <dnsngu@gmail.com>
2020-12-17 19:47:57 +0900

    ch01 : Assertion - Exception 테스트
    issue #1

--- src/test/java/dev/solar/mythejavatest/StudyTest.java
+++ src/test/java/dev/solar/mythejavatest/StudyTest.java
@@ -9,12 +9,7 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기 ╯°□°）╯ ")
     void create_new_study() {
-        Study study = new Study(-10);
-        assertAll(
-                () -> assertNotNull(study),
-                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 " + StudyStatus.DRAFT + "여야 한다."),
-                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
-        );
+        assertThrows(IllegalArgumentException.class, () -> new Study(-10));
     }
 
     @Test
