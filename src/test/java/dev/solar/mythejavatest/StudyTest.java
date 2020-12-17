46939a23fee3811d741a4d8fe93d83738fc302ae
blossun <dnsngu@gmail.com>
2020-12-17 23:42:32 +0900

    ch01 : 태깅과 필터링
    테스트 그룹을 만들고 원하는 테스트 그룹만 테스트를 실행할 수 있는 기능.
    @Tag
    * 테스트 메소드에 태그를 추가할 수 있다.
    * 하나의 테스트 메소드에 여러 태그를 사용할 수 있다.
    
    issue #1

--- src/test/java/dev/solar/mythejavatest/StudyTest.java
+++ src/test/java/dev/solar/mythejavatest/StudyTest.java
@@ -1,38 +1,25 @@
 package dev.solar.mythejavatest;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.condition.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
     @Test
-    @DisplayName("스터디 만들기 ╯°□°）╯ ")
-    @EnabledOnOs({OS.MAC, OS.LINUX}) //해당 운영체제애서만 활성화
-    @EnabledOnJre({JRE.JAVA_8, JRE.JAVA_9, JRE.JAVA_10, JRE.JAVA_11}) //해당 Java 버전환경에서만 활성화
-    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "SOLAR")
+    @DisplayName("스터디 만들기 fast")
+    @Tag("fast")
     void create_new_study() {
-        String test_env = System.getenv("TEST_ENV");
-        System.out.println("TEST_ENV : " + test_env);
-
         Study actual = new Study(10);
         assertThat(actual.getLimit()).isGreaterThan(0);
     }
 
     @Test
-    @DisplayName("스터디 만들기 \uD83D\uDE31")
-    @EnabledOnJre(JRE.OTHER) //JRE.OTHER : 상수로 지정된 값이 아닌 경우
-    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "LOCAL")
+    @DisplayName("스터디 만들기 slow")
+    @Tag("slow")
     void create_new_study_again() {
         Study actual = new Study(10);
         assertThat(actual.getLimit()).isGreaterThan(0);
-    }
-
-    @Test
-    @DisabledOnOs(OS.MAC) //해당 운영체제에서 비활성화
-    void create2() {
-        System.out.println("create2");
     }
 
     @BeforeAll
