30fabd79439dd1a27e9ba2f8e401045ae6e43254
blossun <dnsngu@gmail.com>
2020-12-18 18:38:14 +0900

    ch01 : 테스트 반복하기 - 인자 값 타입 변환
    1. 암묵적인 타입 변환
    인자값의 타입을 변환해주는 인터페이스가 암묵적으로 타입을 변환해준다.
    ex) LocalDate
    2. 명시적인 타입 변환
       (1) SimpleArgumentConverter 상속 받은 구현체를 생성해서 제공
       (2) assertEquals(내가 변환하려는 타입, 타겟 타입, 실패시 메시지)
       (3) 반환값 : Study에 `int limit`을 전달받아 생성하는 생성자가 있기때문에 이를 이용해서 변환
       (4) `@ConvertWith` : 커스텀한 Converter가 어떤 것인지 지정
    
    issue #1

--- src/test/java/dev/solar/mythejavatest/StudyTest.java
+++ src/test/java/dev/solar/mythejavatest/StudyTest.java
@@ -2,9 +2,16 @@ package dev.solar.mythejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
@@ -29,11 +36,27 @@ class StudyTest {
                 repetitionInfo.getTotalRepetitions());
     }
 
-    @DisplayName("반복 테스트 2 - 다른 매개변수")
+    @DisplayName("반복 테스트 2 - 암시적 변환")
+    @ParameterizedTest
+    @ValueSource(strings = {"2017-03-14", "2020-01-20"})
+    void testWith_implicit_conversion(LocalDate localDate) {
+        System.out.println("localDate => "+localDate.toString());
+        assertTrue(localDate != null);
+    }
+
+    @DisplayName("반복 테스트 3 - 명시적 변환")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
-    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
-    void parameterizedTest(String message) {
-        System.out.println(message);
+    @ValueSource(ints = {10, 20, 40})
+    void parameterizedTest(@ConvertWith(StudyConverter.class) Study study) {
+        System.out.println(study.getLimit());
+    }
+
+    public static class StudyConverter extends SimpleArgumentConverter {
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
     }
 
     @BeforeAll
