126304b148379f8d3559292abba323216805fa9a
blossun <dnsngu@gmail.com>
2020-12-18 15:46:27 +0900

    ch01 : 테스트 반복하기 -  RepetitionTest and ParameterizedTest
    @RepeatedTest : 반복 횟수와 반복 테스트 이름을 설정할 수 있다.
    @ParameterizedTest : 테스트에 여러 다른 매개변수를 대입해가며 반복 실행한다.
    
    issue #1

--- src/test/java/dev/solar/mythejavatest/StudyTest.java
+++ src/test/java/dev/solar/mythejavatest/StudyTest.java
@@ -1,6 +1,8 @@
 package dev.solar.mythejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,6 +20,20 @@ class StudyTest {
     void create_new_study_again() {
         Study actual = new Study(10);
         assertThat(actual.getLimit()).isGreaterThan(0);
+    }
+
+    @DisplayName("반복 테스트")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" +
+                repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("반복 테스트 2 - 다른 매개변수")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요."})
+    void parameterizedTest(String message) {
+        System.out.println(message);
     }
 
     @BeforeAll
