03c5cd53d4e959d45be20d3c1f85f1410f133b65
blossun <dnsngu@gmail.com>
2020-12-21 15:35:03 +0900

    refactor : Extention model - [테스트 라이프사이클 콜백]으로 확장팩 생성
    중복 코드 메서드로 추출해서 리팩토링
    
    issue #1

--- src/test/java/dev/solar/mythejavatest/FindSlowTestExtension.java
+++ src/test/java/dev/solar/mythejavatest/FindSlowTestExtension.java
@@ -9,21 +9,25 @@ public class FindSlowTestExtension implements BeforeTestExecutionCallback, After
 
     @Override
     public void beforeTestExecution(ExtensionContext extensionContext) throws Exception {
-        String testClassName = extensionContext.getRequiredTestClass().getName();
-        String testMethodName = extensionContext.getRequiredTestMethod().getName();
-        ExtensionContext.Store store = extensionContext.getStore(ExtensionContext.Namespace.create(testClassName, testMethodName));
+        ExtensionContext.Store store = getStore(extensionContext);
         store.put("START_TIME", System.currentTimeMillis());
     }
 
     @Override
     public void afterTestExecution(ExtensionContext extensionContext) throws Exception {
-        String testClassName = extensionContext.getRequiredTestClass().getName();
         String testMethodName = extensionContext.getRequiredTestMethod().getName();
-        ExtensionContext.Store store = extensionContext.getStore(ExtensionContext.Namespace.create(testClassName, testMethodName));
+        ExtensionContext.Store store = getStore(extensionContext);
         long start_time = store.remove("START_TIME", long.class);
         long duration = System.currentTimeMillis() - start_time;
         if (duration > THRESHOLD) {
             System.out.printf("Please consider mark method [%s] with @SlowTest.\n", testMethodName);
         }
     }
+
+    private ExtensionContext.Store getStore(ExtensionContext extensionContext) {
+        String testClassName = extensionContext.getRequiredTestClass().getName();
+        String testMethodName = extensionContext.getRequiredTestMethod().getName();
+        return extensionContext.getStore(ExtensionContext.Namespace.create(testClassName, testMethodName));
+    }
+
 }
