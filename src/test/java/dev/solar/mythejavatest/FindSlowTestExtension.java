0a3e8d254c1dc5c02fdd5673ec574c0259edec48
blossun <dnsngu@gmail.com>
2020-12-21 15:32:24 +0900

    ch01 : Extention model - [테스트 라이프사이클 콜백]으로 확장팩 생성
    실행하는데 오래 걸리는 테스트를 찾아서  `@SlowTest` 애노테이션이 붙어있지 않은 경우, 해당 애노테이션을 붙이도록 권고(메시지 출력)하는 확장팩
    
    issue #1

---
+++ src/test/java/dev/solar/mythejavatest/FindSlowTestExtension.java
@@ -0,0 +1,29 @@
+package dev.solar.mythejavatest;
+
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class FindSlowTestExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+    private static final long THRESHOLD = 1000L;
+
+    @Override
+    public void beforeTestExecution(ExtensionContext extensionContext) throws Exception {
+        String testClassName = extensionContext.getRequiredTestClass().getName();
+        String testMethodName = extensionContext.getRequiredTestMethod().getName();
+        ExtensionContext.Store store = extensionContext.getStore(ExtensionContext.Namespace.create(testClassName, testMethodName));
+        store.put("START_TIME", System.currentTimeMillis());
+    }
+
+    @Override
+    public void afterTestExecution(ExtensionContext extensionContext) throws Exception {
+        String testClassName = extensionContext.getRequiredTestClass().getName();
+        String testMethodName = extensionContext.getRequiredTestMethod().getName();
+        ExtensionContext.Store store = extensionContext.getStore(ExtensionContext.Namespace.create(testClassName, testMethodName));
+        long start_time = store.remove("START_TIME", long.class);
+        long duration = System.currentTimeMillis() - start_time;
+        if (duration > THRESHOLD) {
+            System.out.printf("Please consider mark method [%s] with @SlowTest.\n", testMethodName);
+        }
+    }
+}
