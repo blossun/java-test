ad42255e6363d4fbaceb3a7b4b7f93222bedb81c
blossun <dnsngu@gmail.com>
2020-12-22 12:33:59 +0900

    ch02: Stubbing - 특정한 매개변수를 받은 경우 특정한 값을 리턴
    Argument matchers를 이용해서 특정한 매개변수가 아닌 범용적인 값을 지정할 수 있다.
    * any() : 어떠한 값이 들어오던지
    
    issue #2

--- src/test/java/dev/solar/mythejavatest/Study/StudyServiceTest.java
+++ src/test/java/dev/solar/mythejavatest/Study/StudyServiceTest.java
@@ -1,5 +1,6 @@
 package dev.solar.mythejavatest.Study;
 
+import dev.solar.mythejavatest.domain.Member;
 import dev.solar.mythejavatest.member.MemberService;
 import dev.solar.mythejavatest.study.StudyRepository;
 import dev.solar.mythejavatest.study.StudyService;
@@ -8,13 +9,29 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
     @Test
     void createStudyService(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("solar@test.com");
+
+        //PMcok Stubbing
+        //memberService.findById(1L)를 했을 때, 앞서정의한 member를 반환해라
+        //1L이 아닌 값으로 findById()를 호출하는 경우에는 반환하지 않음
+        when(memberService.findById(1L)).thenReturn(Optional.of(member));
+
+        Optional<Member> findById = memberService.findById(1L);
+        assertEquals("solar@test.com", findById.get().getEmail());
+
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
     }
