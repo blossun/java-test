a9ada6e8d91516b9c67f9d25628c783df41c5f2d
blossun <dnsngu@gmail.com>
2020-12-22 12:46:27 +0900

    ch02: Stubbing - Void 메소드 특정 매개변수를 받거나 호출된 경우 예외를 발생
    doThrow()
    
    issue #2

--- src/test/java/dev/solar/mythejavatest/Study/StudyServiceTest.java
+++ src/test/java/dev/solar/mythejavatest/Study/StudyServiceTest.java
@@ -3,17 +3,13 @@ package dev.solar.mythejavatest.Study;
 import dev.solar.mythejavatest.domain.Member;
 import dev.solar.mythejavatest.member.MemberService;
 import dev.solar.mythejavatest.study.StudyRepository;
-import dev.solar.mythejavatest.study.StudyService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -25,15 +21,13 @@ class StudyServiceTest {
         member.setEmail("solar@test.com");
 
         //PMcok Stubbing
-        //memberService.findById(1L)를 했을 때, 앞서정의한 member를 반환해라
-        //1L이 아닌 값으로 findById()를 호출하는 경우에는 반환하지 않음
-        when(memberService.findById(1L)).thenReturn(Optional.of(member));
+        //memberService.validate(1)이 호출되면 예외 발생
+        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+        assertThrows(IllegalArgumentException.class, () -> {
+            memberService.validate(1L);
+        });
+        memberService.validate(2L);
 
-        Optional<Member> findById = memberService.findById(1L);
-        assertEquals("solar@test.com", findById.get().getEmail());
-
-        StudyService studyService = new StudyService(memberService, studyRepository);
-        assertNotNull(studyService);
     }
 
 }
